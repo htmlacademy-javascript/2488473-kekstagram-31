0a2c91cb87cc382730b6df94c17f1970e96273c3
x6mode <x6modee@gmail.com>
2024-03-10 11:56:19 +0300

    Проверка

--- js/renderModal.js
+++ js/renderModal.js
@@ -7,25 +7,32 @@ const commentLoader = document.querySelector('.comments-loader');
 const modalPic = document.querySelector('.big-picture__img').getElementsByTagName('img')[0];
 
 let currentIndex = 0;
+let globalPicList = {};
+let globalIndex = 0;
 
 const onCancelClick = () => {
   currentIndex = 0;
+  commentLoader.removeEventListener('click', onLoaderClick);
   picModal.classList.add('hidden');
 }
 
 const onCancelKeyDown = (e) => {
   currentIndex = 0;
-  if (e.code == 'Escape') { picModal.classList.add('hidden') };
+  if (e.code == 'Escape') { picModal.classList.add('hidden'); commentLoader.removeEventListener('click', onLoaderClick);};
+}
+
+const onLoaderClick = () => {
+  const slicedArray = sliceComment(globalPicList[globalIndex].comments);
+  generateComment(slicedArray, currentIndex);
+  currentIndex++;
+  commentCount.text = `${getCountComment()}`;
 }
 
 const getCountComment = () => {
   return document.querySelectorAll('.social__comment').length;
 }
 
-console.log(getCountComment())
-
 const generateComment = (slicedArray, index) => {
-  console.log(index, slicedArray)
   for (let comment of slicedArray[index]) {
     const elemComment = document.createElement("li");
     elemComment.classList.add('social__comment');
@@ -48,7 +55,7 @@ const generateComment = (slicedArray, index) => {
 const sliceComment = (array) => {
   let currentSlice = 0;
   let outputArray = [];
-  for (let i = 0; i <= array.length / 5; i++) {
+  for (let i = 1; i <= array.length / 5; i++) {
     outputArray.push(array.slice(currentSlice, currentSlice + 5));
     currentSlice += 5;
   }
@@ -56,6 +63,7 @@ const sliceComment = (array) => {
 }
 
 const createModal = (picList) => {
+  globalPicList = picList;
   const picMini = document.querySelectorAll('.picture');
   commentCount.text = `${getCountComment()}`;
   for (let [index, item] of picMini.entries()) {
@@ -71,14 +79,10 @@ const createModal = (picList) => {
       currentIndex++;
       commentCount.text = `${getCountComment()}`;
 
-      document.body.classList.add('modal-open');
+      document.body.classList.add('modal-open');  
 
-      commentLoader.addEventListener('click', () => {
-        const slicedArray = sliceComment(picList[index].comments);
-        generateComment(slicedArray, currentIndex);
-        currentIndex++;
-        commentCount.text = `${getCountComment()}`;
-      });
+      globalIndex = index;
+      commentLoader.addEventListener('click', onLoaderClick);
 
     const picCancel = document.querySelector('#picture-cancel');
     picCancel.focus();
