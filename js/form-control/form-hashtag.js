ea932812096bc9e3302bd60e1403bd858ef825cf
x6mode <x6modee@gmail.com>
2024-03-15 12:56:29 +0300

    правки

--- js/form-control/form-hashtag.js
+++ js/form-control/form-hashtag.js
@@ -2,12 +2,9 @@ const regexp = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const inputHashtag = document.querySelector('.text__hashtags');
 
-const checkHashtagsLength = () => hashtags.length <= 5;
+// const checkHashtagsLength = () => hashtags.length <= 5;
 
-const isValidHashtag = (evt) => {
-  const hashtags = evt.target.value.split(' ').filter((el) => el !== '' && el.startsWith('#') && regexp.test(el));
-  console.log(hashtags);
-};
+const isValidHashtag = (evt) => evt.target.value.split(' ').filter((el) => el !== '' && el.startsWith('#') && regexp.test(el));
 
 const loadHashtagValidator = () => {
   inputHashtag.addEventListener('input', isValidHashtag);
