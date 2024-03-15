b7a82fa6b082fa10cf6d5a3eda2666388d5eb05c
x6mode <x6modee@gmail.com>
2024-03-15 12:04:38 +0300

    добавление фильтров

--- js/form-control/form-comment.js
+++ js/form-control/form-comment.js
@@ -1,6 +1,20 @@
 
-// const inputComment = document.querySelector('.text__description');
+const inputComment = document.querySelector('.text__description');
 
-// const loadCommentValidator = () => {
-//   inputComment.addEventListener
-// };
+const checkLength = (evt) => {
+  if (evt.target.value.length <= 140) {
+    console.log('ok');
+  } else {
+    console.log('dont ok');
+  };
+};
+
+const loadCommentValidator = () => {
+  inputComment.addEventListener('input', checkLength);
+};
+
+const unloadCommentValidator = () => {
+  inputComment.removeEventListener('input', checkLength);
+};
+
+export { loadCommentValidator, unloadCommentValidator };
--- js/form-control/form-hashtag.js
+++ js/form-control/form-hashtag.js
@@ -2,13 +2,12 @@ const regexp = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const inputHashtag = document.querySelector('.text__hashtags');
 
-const getHashtags = () => inputHashtag.value.split(' ');
-const hashtags = getHashtags();
 const checkHashtagsLength = () => hashtags.length <= 5;
 
-const checkHashtag = (hashtag) => regexp.test(hashtag) && hashtag.endsWith(' ');
-
-const isValidHashtag = () => hashtags.forEach(checkHashtag);
+const isValidHashtag = (evt) => {
+  const hashtags = evt.target.value.split(' ').filter((el) => el !== '' && el.startsWith('#') && regexp.test(el));
+  console.log(hashtags);
+};
 
 const loadHashtagValidator = () => {
   inputHashtag.addEventListener('input', isValidHashtag);
