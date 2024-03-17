19ff5332c4595e437457e671799c17509db29faa
Keks <keks@htmlacademy.ru>
2024-03-17 17:39:08 +0300

    :heavy_check_mark: Сборка #17

---
+++ 17/js/form-control/form-comment.js
@@ -0,0 +1,21 @@
+import { pristineFormOptions } from '../utils.js';
+
+
+// const inputComment = document.querySelector('.text__description');
+//
+// const loadCommentValidator = () => {
+//   inputComment.addEventListener
+// };
+
+const inputComment = document.querySelector('.text__description');
+const pristine = new Pristine(document.querySelector('.img-upload__form'), pristineFormOptions);
+
+const checkLength = (value) => value.length <= 140;
+
+const loadCommentValidator = () => {
+  pristine.reset();
+  pristine.addValidator(inputComment, checkLength, 'Длина комментария больше 140 символов', false);
+};
+
+
+export { loadCommentValidator, unloadCommentValidator };
