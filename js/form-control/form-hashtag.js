9f992b06b67e0cb3d2cfae7a5ef491eed6457cef
x6mode <x6modee@gmail.com>
2024-03-17 17:15:37 +0300

    Валидация хештегов + коментариев через pristine.js

--- js/form-control/form-hashtag.js
+++ js/form-control/form-hashtag.js
@@ -1,13 +1,21 @@
+import { pristineFormOptions } from '../utils.js';
+
 const regexp = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const inputHashtag = document.querySelector('.text__hashtags');
+const pristine = new Pristine(document.querySelector('.img-upload__form'), pristineFormOptions);
 
-// const checkHashtagsLength = () => hashtags.length <= 5;
+const checkHashtagsLength = (array) => array.trim().split(' ').length <= 5;
 
-const isValidHashtag = (evt) => evt.target.value.split(' ').filter((el) => el !== '' && el.startsWith('#') && regexp.test(el));
+const checkEveryHashtag = (array) => array.trim().split(' ').every((element) => element.startsWith('#') && regexp.test(element) && element !== '#');
+
+const checkDuplicate = (array) => (new Set(array.trim().split(' ')).size === array.trim().split(' ').length);
 
 const loadHashtagValidator = () => {
-  inputHashtag.addEventListener('input', isValidHashtag);
+  pristine.reset();
+  pristine.addValidator(inputHashtag, checkHashtagsLength, 'Превышено количество хэштегов', false);
+  pristine.addValidator(inputHashtag, checkEveryHashtag, 'Введён невалидный хэштег', false);
+  pristine.addValidator(inputHashtag, checkDuplicate, 'Хэштеги повторяются', false);
 };
 
 export { loadHashtagValidator };
