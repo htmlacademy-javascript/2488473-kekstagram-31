165a9bb5f1f5c7fa2809810a3c8fbdfd3de7970f
Keks <keks@htmlacademy.ru>
2024-03-21 18:52:10 +0300

    :heavy_check_mark: Сборка #19

--- 19/js/server-action/connect.js
+++ 19/js/server-action/connect.js
@@ -1,8 +1,4 @@
 
-const getServerData = (onSuccess) => {
- return fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
-  .then((response) => response.json())
-  .then((data) => onSuccess(data));
-};
+const getServerData = (onSuccess) => fetch('https://31.javascript.htmlacademy.pro/kekstagram/data').then((response) => response.json()).then((data) => onSuccess(data));
 
 export { getServerData };
