c9e4b8d4d124d2821abda61d6ca41068dc4ad4e5
x6mode <x6modee@gmail.com>
2024-03-21 18:53:41 +0300

    Правка недобавленных файлов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,10 @@
 import { createModal } from './render-modal.js';
-import { createPictures, picList } from './render.js';
+import { loadPicture } from './render.js';
 import { loadFormControl } from './form-control/form-control.js';
+import { getServerData } from './server-action/connect.js';
 
-createPictures();
-createModal(picList);
+
+loadPicture();
+getServerData(createModal);
 loadFormControl();
+
