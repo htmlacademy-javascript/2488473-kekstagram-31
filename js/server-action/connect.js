0f7439efaffa7f8ae8e9df96a35d32be109f90fa
x6mode <x6modee@gmail.com>
2024-04-07 20:38:21 +0300

    Правка

--- js/server-action/connect.js
+++ js/server-action/connect.js
@@ -1,6 +1,6 @@
 import { alertLoadError } from '../utils.js';
 
-const getServerData = () => fetch('https://3.javascript.htmlacademy.pro/kekstagram/data')
+const getServerData = () => fetch('https://31.javascript.htmlacademy.pro/kekstagram/data')
   .then((response) => {
     if (response.ok) {
       document.querySelector('.img-filters--inactive').classList.remove('img-filters--inactive');
